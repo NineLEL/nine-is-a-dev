077a4734a4c8c1c6fe6351d111c3699480ee2c2a
NineLEL <thanatnan9@gmail.com>
2025-04-01 20:51:10 +0700

    Optimize Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vue from '@astrojs/vue';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
-
+  integrations: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => tag.startsWith('swiper-')
+        }
+      },
+      reactivityTransform: true
+    })
+  ],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ['vue']
+    }
   }
 });
